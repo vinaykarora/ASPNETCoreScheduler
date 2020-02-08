c18918b683a9ac97876452905b3421df2b5bd7ee
Vinay Arora <vinay.arora7171@hotmail.com>
2020-02-08 18:54:57 +0530

    Adding Background Services and Scheduler

---
+++ ASPNETCoreScheduler/Scheduler/ScheduleTask.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ASPNETCoreScheduler.Scheduler
+{
+    public class ScheduleTask : ScheduledProcessor
+    {
+
+        public ScheduleTask(IServiceScopeFactory serviceScopeFactory) : base(serviceScopeFactory)
+        {
+        }
+
+        protected override string Schedule => "*/10 * * * *";
+
+        public override Task ProcessInScope(IServiceProvider serviceProvider)
+        {
+            Console.WriteLine("Processing starts here");
+            return Task.CompletedTask;
+        }
+    }
+}
